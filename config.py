b737e5a7153d9ff1a516e0ba165aa5da80dc7b6c
Haritoh <88691406+Haritoh@users.noreply.github.com>
2022-11-13 20:41:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_ID =  os.environ.get('api_id','')
 API_HASH = os.environ.get('api_hash','')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','Haroldgo09')
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KADKFLCAJDJJKJJGDYFASDDJHNJGDADKDGLGDELI'))
 
 if PROXY:
