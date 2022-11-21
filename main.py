7c50961a0606ecc2e65508da0caa9a97baf1cb5d
Haritoh <88691406+Haritoh@users.noreply.github.com>
2022-11-21 12:36:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -167,8 +167,8 @@ async def onmessage(bot:TelegramClient,ev: NewMessage.Event,loop,ret=False):
     username = ev.message.chat.username
     text = ev.message.text
 
-    #if username not in config.ACCES_USERS:
-    if username not in tl_admin_users:
+    if username not in config.ACCES_USERS:
+    #if username not in tl_admin_users:
         await bot.send_message(ev.chat.id,'🛑No Tiene Acceso🛑')
         return
 
